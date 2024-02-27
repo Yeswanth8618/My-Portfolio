04263f78fdd8b81895d1665c5f68bfff7fa4a2a3
yeswanth T <113173144+Yeswanth8618@users.noreply.github.com>
2024-02-27 14:43:09 +0530

    Add files via upload

---
+++ my profile/script.js
@@ -0,0 +1,27 @@
+let element=document.getElementById('hh')
+
+// let type=()=>{
+//     let name="Yeswanth"
+//     let index=0;
+//     let typeinterval=setInterval(() => {
+//         element.textContent=name.slice(0,index)
+//         index++
+//         if(index>name.length){
+//             clearInterval(typeinterval)
+//             setTimeout(erase,1000)
+//         }
+//     }, 100);
+// }
+// let erase=()=>{
+//     let name="Yeswanth"
+//     let index=name.length;
+//     let eraseinterval=setInterval(() => {
+//         element.textContent=name.slice(0,index)
+//         index--
+//         if(index===0){
+//             clearInterval(eraseinterval)
+//             setTimeout(type,500)
+//         }
+//     }, 50);
+// }
+// type()
